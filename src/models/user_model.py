c6c34ae3101c5f2d23ab252dd54e0db8878741be
Luis Obando <110127391+FrostLord98@users.noreply.github.com>
2024-07-08 23:25:58 -0400

    Update user_model.py

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,7 +1,10 @@
+import pytz
+from datetime import datetime #esto nos permite adjuntar la fecha y hora de creacion del usuario
+
 #se crea la clase User para manejar de forma mas eficiente la informacion
 class User():
     
-    def __init__(self, id=0, username = 0, password_hash = 0, email = 0,created_at = 0) -> None:
+    def __init__(self, id=0, username = 0, password_hash = 0, email = 0,created_at = lambda: datetime.now(pytz.timezone("America/Caracas"))) -> None:
         self.id = id
         self.username = username
         self.password_hash = password_hash
